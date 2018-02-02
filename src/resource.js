b183818e194785aaa28627173f4f8bd8cb40fba8
Fabio Elizandro <fabio@elizandro.com.br>
2018-02-03 01:46:07 +1100

    fix(resource): dont try parse to json when no content response

--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,16 @@
+const NO_CONTENT = 204;
+
 module.exports = fetch => {
-  return url => {
-    return fetch(url).then(response => response.json());
+  return (url, options) => {
+    return fetch(url, options)
+      .then(response => {
+        const status = response.status;
+
+        if (status === NO_CONTENT) {
+          return Promise.resolve(null);
+        }
+
+        return response.json();
+      });
   };
 };
--- src/resource.spec.js
+++ src/resource.spec.js
@@ -1,7 +1,8 @@
 const resource = require('./resource');
 
-const createFetch = () => {
+const createFetch = ({ status = 200 }) => {
   return jest.fn(() => Promise.resolve({
+    status,
     json: () => Promise.resolve({
       class: ['my-siren-resource']
     })
@@ -11,17 +12,31 @@ const createFetch = () => {
 describe('resource', () => {
   it('calls fetch with the right endpoint', async () => {
     expect.assertions(1);
-    const fetch = createFetch();
+    const fetch = createFetch({});
     await resource(fetch)('http://example.com');
-    expect(fetch).toHaveBeenCalledWith('http://example.com');
+    expect(fetch).toHaveBeenCalledWith('http://example.com', undefined);
   });
 
   it('returns an resource', async () => {
     expect.assertions(1);
-    const fetch = createFetch();
+    const fetch = createFetch({});
     const response = await resource(fetch)('http://example.com');
     expect(response).toEqual({
       class: ['my-siren-resource']
     });
+  });
+
+  it('pass options to fetch the resource', async () => {
+    expect.assertions(1);
+    const fetch = createFetch({});
+    await resource(fetch)('http://example.com', { foo: 'bar' });
+    expect(fetch).toHaveBeenCalledWith(expect.any(String), { foo: 'bar' });
+  });
+
+  it('returns null when response has no content', async () => {
+    expect.assertions(1);
+    const fetch = createFetch({ status: 204 });
+    const response = await resource(fetch)('http://example.com');
+    expect(response).toEqual(null);
   });
 });
